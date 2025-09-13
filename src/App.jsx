da1daf913ce137224b3e7abe52f1fbe458b3d4ef
Ajsa <besirovicajsa@gmail.com>
2025-09-13 19:43:20 +0200

    3.cas i domaci

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
 import "./App.css";
+import Header from "./Header.jsx";
 import Message from "./Message.jsx";
-// import koristimo da ubacimo komponentu u drugi fajl
+import UserCard from "./UserCard.jsx";
 
 function App() {
+  const users = [
+    { id: 1, name: "ajsa", age: 21 },
+    { id: 2, name: "jana", age: 20 },
+  ];
+
+  // Ovde za svakog korisnika iz niza users kreiramo UserCard komponentu
+  // i šaljemo joj podatke kao ATRIBUTE (name i age)
+  // Npr. <UserCard name="ajsa" age={21} />
+  const content = users.map((user) => (
+    <UserCard name={user.name} age={user.age} />
+  ));
+
   return (
-    <div>
-      <h1>ajsa besirovic</h1>
+    <>
+      <Header />
       <Message />
-    </div>
+
+      {/* Primer: prikazujemo listu UserCard komponenti */}
+      {users.map((user, index) => (
+        <UserCard name={user.name} age={user.age} key={index} />
+      ))}
+
+      {/* Napomena o key-u:
+         - Key je obavezan kada renderujemo listu elemenata.
+         - Ako lista nikada ne menja svoj sadržaj, može se koristiti index.
+         - Ako lista može da se menja (dodavanje, brisanje), bolje je koristiti jedinstveni ID. 
+      */}
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+          // Ovde koristimo "id" jer je jedinstven za svakog korisnika.
+          // React tada može da prati elemente na ispravan način.
+        ))}
+      </ul>
+    </>
   );
 }
-// Komponenta mora da vrati tačno jedan glavni element
-// Ako želimo više elemenata, moramo ih uvući u jedan zajednički (npr. <div> ili fragment <> </>)
 
 export default App;
